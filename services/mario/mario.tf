9b5885e1f7e3b1efd3429f1b3def09db3e4737b2
jperez3 <joe@ordisius.com>
2022-03-12 16:32:47 -0600

    changing service name

--- services/mario/mario.tf
+++ services/mario/mario.tf
@@ -1,14 +1,14 @@
-module "mario" {
-  source = "git::git@github.com:jperez3/terraform-bootstrap.git//service?ref=main"
+module "burrito" {
+  source = "git::git@github.com:jperez3/terraform-bootstrap.git//?ref=main"
 
   env     = "prod"
-  service = "mario"
+  service = "burrito"
 }
 
 output "ssh_clone_url" {
-    value = module.mario.ssh_clone_url
+    value = module.burrito.ssh_clone_url
 }
 
 output "gha_url" {
-    value = module.mario.gha_url
+    value = module.burrito.gha_url
 }
